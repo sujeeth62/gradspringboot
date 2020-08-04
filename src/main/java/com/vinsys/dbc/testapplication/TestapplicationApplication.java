4d3b4646ce9368daa6c68fc117c7ddefc9291bf3
nilesh.devdas <nilesh.devdas@vinsys.com>
2020-08-04 07:09:07 +0530

    Initial Commit

---
+++ src/main/java/com/vinsys/dbc/testapplication/TestapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.vinsys.dbc.testapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestapplicationApplication.class, args);
+	}
+
+}
